ce3b65af9381bf137dbf95d77be139c1263cdc38
Zipeng Liang <zipengliang21@gmail.com>
2022-06-28 11:22:48 -0700

    Initial Project Add

---
+++ src/main/java/guru/springframework/zipengpetclinic/ZipengPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.zipengpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipengPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipengPetClinicApplication.class, args);
+    }
+
+}
