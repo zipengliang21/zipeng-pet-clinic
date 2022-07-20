51dceebd619f4c3b6f399f26edee70cc9d8302e2
Zipeng Liang <zipengliang21@gmail.com>
2022-07-20 15:58:59 -0700

    Impl Map based Services, Closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/zipengpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.zipengpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.zipengpetclinic.model.Pet;
+import guru.springframework.zipengpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/zipengpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.zipengpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.zipengpetclinic.model.Vet;
+import guru.springframework.zipengpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
